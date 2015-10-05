d02ddc10c36f2557ef0526431aad1bf5bfa3f1a0
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-05 09:56:19 +0200

    Dependent cookbooks pulled into default recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,13 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
+include_recipe "chef-bioinf-worker::openms"
+include_recipe "chef-bioinf-worker::crux"
+include_recipe "chef-bioinf-worker::tandem"
+include_recipe "chef-bioinf-worker::blast"
+include_recipe "chef-bioinf-worker::omssa"
+include_recipe "chef-cuneiform::default"
+
 directory node.dir.wf
 directory node.dir.data
 
