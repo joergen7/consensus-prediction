340bcbd91ce735fe086f08f1d92eff97079461c1
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-05 11:38:52 +0200

    Metadata updated.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,14 @@
 name             'consensus-prediction'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
+maintainer       "Jörgen Brandt"
+maintainer_email "brandjoe@hu-berlin.de"
 license          'all_rights'
 description      'Installs/Configures consensus-prediction'
 long_description 'Installs/Configures consensus-prediction'
 version          '0.1.0'
 
+recipe "consensus-prediction::_common", "Performs an update of the package system."
+recipe "consensus-prediction::default", "Sets up all preconditions to run the consensus prediction workflow."
+
+
+depends "chef-bioinf-worker"
+depends "chef-cuneiform"
