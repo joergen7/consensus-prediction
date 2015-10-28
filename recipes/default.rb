eb056ac8f32d61d3bea42b2a53c4310f096ff635
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-28 14:18:52 +0100

    Input data altered.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,17 @@
+# coding: utf-8
 #
 # Cookbook Name:: consensus-prediction
 # Recipe:: default
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
+
+
+spectra_id_set = ["NUNP1", "NUNP2", "ORG1", "ORG2"]
+spectra_dir = "#{node.dir.data}/spectra"
+
+pep_id_set = ["abinitio", "all"]
+pep_dir = "#{node.dir.data}/pep"
+
 
 include_recipe "chef-bioinf-worker::openms"
 include_recipe "chef-bioinf-worker::msgf"
@@ -13,19 +22,41 @@ include_recipe "chef-cuneiform::default"
 
 directory node.dir.wf
 directory node.dir.data
+directory spectra_dir
+directory pep_dir
 
 # place workflow
 template "#{node.dir.wf}/consensus.cf" do
   source "consensus.cf.erb"
 end
 
-# place example data
-cookbook_file "#{node.dir.data}/spectra.tar" do
-  source "spectra.tar"
-end
 
-cookbook_file "#{node.dir.data}/barleyprots.fa" do
-  source "barleyprots.fa"
-end
+# download mgf spectra
+spectra_id_set.each { |id|
+
+  link = "http://www.ebi.ac.uk/pride/data/archive/2015/08/PXD001933/#{id}.mgf"
+  file = "#{spectra_dir}/#{File.basename( link )}"
+
+  remote_file file do
+    action :create_if_missing
+    source link
+    retries 1
+  end
+
+}
+
+# download peptide sequences
+pep_id_set.each { |id|
+
+  link = "ftp://ftp.ensembl.org/pub/release-82/fasta/homo_sapiens/pep/Homo_sapiens.GRCh38.pep.#{id}.fa.gz"
+  file = "#{pep_dir}/#{File.basename( link )}"
+
+  remote_file file do
+    action :create_if_missing
+    source link
+    retries 1
+  end
+
+}
 
 
